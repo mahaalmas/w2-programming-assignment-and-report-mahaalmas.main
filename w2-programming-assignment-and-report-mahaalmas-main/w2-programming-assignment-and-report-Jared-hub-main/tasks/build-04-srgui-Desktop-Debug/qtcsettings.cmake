dc0c1a27dadd5133c097b075c4bbbc3cdb40fe74
maha almasoudi <146206593+mahaalmas@users.noreply.github.com>
2024-01-10 14:26:41 +0000

    Add files via upload

---
+++ w2-programming-assignment-and-report-mahaalmas-main/w2-programming-assignment-and-report-Jared-hub-main/tasks/build-04-srgui-Desktop-Debug/qtcsettings.cmake
@@ -0,0 +1,9 @@
+# This file is managed by Qt Creator, do not edit!
+
+set("CMAKE_GENERATOR" "Ninja" CACHE "STRING" "" FORCE)
+set("CMAKE_BUILD_TYPE" "Debug" CACHE "STRING" "" FORCE)
+set("CMAKE_PROJECT_INCLUDE_BEFORE" "D:/taskss/JERRY/C++/Qtcreator/w2-programming-assignment-and-report-Jared-hub-main/tasks/build-04-srgui-Desktop-Debug/.qtc/package-manager/auto-setup.cmake" CACHE "FILEPATH" "" FORCE)
+set("QT_QMAKE_EXECUTABLE" "" CACHE "FILEPATH" "" FORCE)
+set("CMAKE_PREFIX_PATH" "" CACHE "PATH" "" FORCE)
+set("CMAKE_C_COMPILER" "C:/msys64/ucrt64/bin/gcc.exe" CACHE "FILEPATH" "" FORCE)
+set("CMAKE_CXX_COMPILER" "C:/msys64/ucrt64/bin/g++.exe" CACHE "FILEPATH" "" FORCE)
